3534132364f405c3d8e5e20aad298386b9bba80a
Javier Medina <javier_j_medina@hotmail.com>
2022-03-22 01:25:09 -0300

    por hooks

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import { Fragment } from 'react'
 import { Popover, Transition } from '@headlessui/react'
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
 
+
 const navigation = [
   { name: 'Product', href: '#' },
   { name: 'Features', href: '#' },
